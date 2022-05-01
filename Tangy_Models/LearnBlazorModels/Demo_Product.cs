8298192e25063c4a18f2e8c3f13e9f82f0b9614c
Dreamy <dreamy26@icloud.com>
2022-05-01 01:14:43 -0400

    adds Data Binding

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Tangy_Models.LearnBlazorModels
+{
+	public class Demo_Product
+	{
+		
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public bool IsActive { get; set; }
+		public double Price { get; set; }
+        public string? ThisProductIs { get; set; }
+        public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
+    }
+}
+
