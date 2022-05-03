660c2004308f69afb381292648e827a22dad87c6
Dreamy <dreamy26@icloud.com>
2022-05-03 00:09:45 -0400

    adds more 2 product list

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 namespace Tangy_Models.LearnBlazorModels
 {
-	public class Demo_Product
-	{
-		
-		public int Id { get; set; }
-		public string? Name { get; set; }
-		public bool IsActive { get; set; }
-		public double Price { get; set; }
+    public class Demo_Product
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsActive { get; set; }
+        public double Price { get; set; }
         public string? ThisProductIs { get; set; }
-        public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
+        public List<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
 
