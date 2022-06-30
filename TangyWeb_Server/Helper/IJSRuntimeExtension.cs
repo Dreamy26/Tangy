3d538aed34892a26a70d0d26d47129e223959272
Dreamy <dreamy26@icloud.com>
2022-06-30 17:45:24 -0400

    build: adds JsRuntime Extensions

---
+++ TangyWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.JSInterop;
+
+namespace TangyWeb_Server.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToaster", "success", message); //frm common.js "type" then message
+        }
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToaster", "error", message); //frm common.js
+        }
+    }
+}
+
