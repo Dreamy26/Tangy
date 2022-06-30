531b04f4f29f0759ca6a4683fbd7e54df2365463
Dreamy <dreamy26@icloud.com>
2022-06-30 17:18:25 -0400

    build: adds additional ToasterJS

---
+++ TangyWeb_Server/wwwroot/Js/common.js
@@ -0,0 +1,8 @@
+﻿window.ShowToaster = (type, message) => {
+    if (type === "success") {
+        toaster.success(message, "Operation Successful", { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toaster.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
