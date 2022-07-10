10ab8c756177beea8923248718a0203ffd3ab74e
Dreamy <dreamy26@icloud.com>
2022-07-09 23:44:00 -0400

    Setup ApplicationDbContext

--- Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace Tangy_DataAccess.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        // ctor, receives DbContext options_class ApplicationDbContext passed to the baseclass of DbContext
+        // default configuration
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<Category> Categories { get; set; } // DbSet_Class Categories / Categories table inside database
     }
+
+
 }
 
