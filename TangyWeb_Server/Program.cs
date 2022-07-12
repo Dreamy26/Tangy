6c47e7d9acbb47b5b18c954b27a8ec9c1b800ea6
Dreamy <dreamy26@icloud.com>
2022-07-12 19:29:23 -0400

    refactor: Deletes projects added to solution

--- TangyWeb_Server/Program.cs
+++ TangyWeb_Server/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+//using Tangy_DataAccess.Data;
 using TangyWeb_Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,6 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
-var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
